1456a0d32a391b6258d9d8656337d0e2e2c486d4
Vasily Kirichenko <vaskir@gmail.com>
2017-12-03 16:28:40 +0300

    init

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿open System.Diagnostics
+open System.Text.RegularExpressions
+open System.IO
+open System
+
+let search file pattern =
+    let r = Regex(pattern, RegexOptions.ExplicitCapture ||| RegexOptions.Compiled)
+    let delims = [|';'; '"'; ' '|]
+    [| for line in File.ReadLines file |> Seq.take 10_000_000 do
+         match r.Match line with
+         | m when m.Success -> 
+             yield m.Value.Split(delims, StringSplitOptions.RemoveEmptyEntries) |> String.concat "."
+         | _ -> () |]
+
+[<EntryPoint>]
+let main _ =
+    let sw = Stopwatch.StartNew()
+    let results = search @"d:\big.txt" @"\{.*(?<name>Microsoft.*)\|\]"
+    sw.Stop()
+    printfn "Found %d lines, Elapsed %O" results.Length sw.Elapsed
+    0
